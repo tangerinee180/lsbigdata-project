c419a30f06ba9febf6b995106278d663ac8ffde9
Yonggyu Lee <tangerinee789@gmail.com>
2024-07-10 17:42:02 +0900

    my first Ds code

---
+++ lec2.py
@@ -0,0 +1,103 @@
+# Cntr + Enter 
+# Shift + 화면 드래그
+a = 1
+a
+#파워셀 명령어 리스트
+#cd 내부에 있는 애  조금 치고 tap 누르면 됨
+#ls 
+#.현재폴더
+#..상위폴더 (cd ..) (cd ../..) -> / 계속 가능
+#clear 화면 정리 cls
+
+
+#Show folder in new window 해당 위치 폴더 열기
+#톱니바퀴에 다 있다.
+#"" '' 둘 다 된다
+a = 5
+a = "안녕하세요!"
+a = '안녕하세요!'
+a = [1, 2, 3]
+var1 = [1, 2, 3]
+var2 = [4,5,6]
+var3 = "안녕하세요!"
+var4 = " "
+var5 = "LS 빅데이터 스쿨!"
+var1 + var2
+var2 + var1
+strsplit(var3,"하")[1]
+var5 + var4 + var3
+#shift + alt +아래화살표 (복사)
+#Ctrl + alt + 아래화살표(커서여러개)
+print(var3)
+c = 6
+d = 4
+
+print("c + d = " , c+d)
+print("c - d = ", c-d)
+print("c * d = ", c*d)
+print("c / d = ", c/d)
+print("c % d = ", c%d)
+print("c ** d = " , c**d)
+
+c = 10
+d = 20
+print("c == d = " , c==d)
+print("c != d = " , c!=d)
+print("c < d = " , c<d)
+print("c > d = " , c>d)
+print("c <= d = " , c<=d)
+print("c >= d = " , c>=d)
+
+sum(var1)
+max(var1, var2)
+max(var1+var2)
+
+
+
+user_age = 25
+is_adult = user_age >= 18
+print("성인입니까?", is_adult)
+
+TRUE = 1
+true = 1
+a = "True"
+b = TRUE
+c = true
+d = True
+
+a = True 
+b = False
+
+a and b
+b and b
+a or b
+not a 
+not b
+b or b 
+
+
+min(a+b,1)
+
+
+a = 24 
+b = 4
+c = 5
+
+a -= 4
+
+a += 12
+a
+
+str1 = "Hello"
+
+repeated_str = str1 * 3
+print("repeated_string = ",repeated_str)
+
+# 정수 : int(integer)
+# 실수 : float(double)
+# 이진수 : binary
+
+x = 4
+bin(x)
+bin(~x)
+
